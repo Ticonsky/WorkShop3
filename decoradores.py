65a6b1e9a44d2b01ec6f11344b8fa5c8cb2bb9b0
Ticonsky <144262984+Ticonsky@users.noreply.github.com>
2024-04-26 09:58:45 -0500

    Update decoradores.py

--- decoradores.py
+++ decoradores.py
@@ -1,7 +1,7 @@
 import time
 import psutil
 import functools
-
+from collections import deque
 def calcular_tiempo(funcion):
         def wrapper(*args, **kwargs):
             inicio = time.time()  # Tiempo inicial
@@ -25,3 +25,34 @@ def calcular_memoria(funcion):
         return resultado
     return wrapper
 
+
+
+def log_funcion(funcion):
+    @functools.wraps(funcion)
+    def wrapper(*args, **kwargs):
+        with open("actions.txt", "a") as file:
+            file.write(f"Llamada a la función {funcion.__name__}\n")
+            file.write(f"Argumentos posicionales: {args}\n")
+            file.write(f"Argumentos de palabras clave: {kwargs}\n\n")
+        return funcion(*args, **kwargs)
+    return wrapper
+
+
+def lru_cache_size(maxsize):
+    cache = {}
+    queue = deque(maxlen=maxsize)
+
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            key = args + tuple(kwargs.items())
+            if key in cache:
+                return cache[key]
+            result = func(*args, **kwargs)
+            if len(queue) == maxsize:
+                del cache[queue.popleft()]
+            cache[key] = result
+            queue.append(key)
+            return result
+        return wrapper
+    return decorator
